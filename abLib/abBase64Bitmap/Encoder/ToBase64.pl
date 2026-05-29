9f51b7cf595a885067e0881090062eda9a14dbe1
Agustín Benítez Hernández <abenitez69@gmail.com>
2026-05-29 13:56:02 -0400

    Add files via upload

---
+++ abLib/abBase64Bitmap/Encoder/ToBase64.pl
@@ -0,0 +1,20 @@
+use MIME::Base64;
+
+my $BinFile = $ARGV[0];
+exit if( $BinFile eq "" );
+
+open( FILE, "demo.jpg" );
+    my $BinLst = join( '', <FILE> );
+close( FILE );
+
+my $MimeBase64 = encode_base64( $BinLst );
+$MimeBase64 =~ s/\n/" &\n\t"/g;
+$MimeBase64 = "myVar = \"" . $MimeBase64 . "\"";
+
+open( FILE, ">bin_to_base64.wxb" );
+    print FILE $MimeBase64;
+close( FILE );
+
+
+
+
